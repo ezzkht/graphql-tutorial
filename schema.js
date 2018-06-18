3f2b14fd06aeea8a3eaba4eb16e058d00ecca214
Ezz Khayyat <ekhayyat@gmail.com>
2018-06-18 14:40:42 +0300

    initial commit

---
+++ schema.js
@@ -0,0 +1,19 @@
+import {makeExecutableSchema, addMockFunctionsToSchema} from 'graphql-tools';
+import  resolvers from './resolvers';
+
+const typeDefs = `type Author {
+  id: String
+  age: Int
+  name: String
+  books: [String]
+}
+
+type Query {
+  authors: [Author]
+  author(id: String): Author
+}`
+
+const schema = makeExecutableSchema({typeDefs, resolvers});
+//addMockFunctionsToSchema({schema});
+
+export default schema;
